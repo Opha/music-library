eaf17fc41e61f00b544741819230d692e0344d74
Opha <avetisianophelia@gmail.com>
2021-02-20 09:30:45 +0400

    util.js updated, song library styled and functionelle added

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Library from "./Library";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ song, songs, setCurrentSong, id }) => {
+  const songSelectHandler = () => {
+    const selectedSong = songs.filter((state) => state.id === id);
+    setCurrentSong(selectedSong[0]);
+  };
+
   return (
-    <div className="library-song">
+    <div className="library-song" onClick={songSelectHandler}>
       <img src={song.cover} alt={song.name} />
       <div className="song-description">
         <h3>{song.name}</h3>
